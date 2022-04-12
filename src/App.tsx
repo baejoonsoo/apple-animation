5db9fc912177893f7c277c5a32aefdbbf235a0cb
baejoonsoo <artns25@dsm.hs.kr>
2022-04-12 21:47:08 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  let canvas: CanvasRenderingContext2D | null;
+  const img = new Image();
+
+  const currentFrame = (index: Number): string =>
+    `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+      .toString()
+      .padStart(4, "0")}.jpg`;
+
+  useEffect(() => {
+    img.src = currentFrame(1);
+    if (canvasRef && canvasRef.current) {
+      canvas = canvasRef.current.getContext("2d");
+      canvasRef.current.width = 1158;
+      canvasRef.current.height = 770;
+    }
+    if (canvas) {
+      canvas.drawImage(img, 0, 0);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas ref={canvasRef} />
     </div>
   );
 }
